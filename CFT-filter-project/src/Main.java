e13646255e1eb6b08415bcc4e10dafe0d6e80e1e
Maxim Kulygin <akademuk97@gmail.com>
2025-07-01 18:30:21 +0700

    Sample filter + multiple output (first version)

--- CFT-filter-project/src/Main.java
+++ CFT-filter-project/src/Main.java
@@ -1,39 +1,47 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
 
+        BufferedReader[] readers = new BufferedReader[args.length];
 
-        List<String> lines = new ArrayList<String>();
-
-
-        try (BufferedReader reader = Files.newBufferedReader(Paths.get(
-                "C:\\Users\\Maximus\\Desktop\\work\\SHIFT Java\\Task\\CFT-filter-utility\\files\\input\\input.txt"))) {
+        try (
+                BufferedReader reader = Files.newBufferedReader(Paths.get(
+                        "C:\\Users\\Maximus\\Desktop\\work\\SHIFT Java\\Task\\CFT-filter-utility\\files\\input\\input.txt"));
+                FileWriter integerWriter = new FileWriter(  // можно ДОБАВЛЯТЬ в файл, применив в конструкторе append
+                        "C:\\Users\\Maximus\\Desktop\\work\\SHIFT Java\\Task\\CFT-filter-utility\\files\\output\\integers.txt");
+                FileWriter floatWriter = new FileWriter(  // можно ДОБАВЛЯТЬ в файл, применив в конструкторе append
+                        "C:\\Users\\Maximus\\Desktop\\work\\SHIFT Java\\Task\\CFT-filter-utility\\files\\output\\floats.txt");
+                FileWriter stringWriter = new FileWriter(  // можно ДОБАВЛЯТЬ в файл, применив в конструкторе append
+                        "C:\\Users\\Maximus\\Desktop\\work\\SHIFT Java\\Task\\CFT-filter-utility\\files\\output\\strings.txt")
+        ) {
             String line;
             while ((line = reader.readLine()) != null) {
-                System.out.println(line);
-                lines.add(line);
+                //System.out.println(line);
+                if (line.matches("^[+-]?\\d+$")) {
+                    integerWriter.write(line);
+                    integerWriter.write(System.lineSeparator()); // Добавляем перевод строки
+                    integerWriter.flush(); // Принудительная запись в файл
+                } else if (line.matches("[+-]?(\\d+(\\.\\d*)?|\\.\\d+)([eE][+-]?\\d+)?")) {
+                    floatWriter.write(line);
+                    floatWriter.write(System.lineSeparator()); // Добавляем перевод строки
+                    floatWriter.flush(); // Принудительная запись в файл
+                } else {
+                    stringWriter.write(line);
+                    stringWriter.write(System.lineSeparator()); // Добавляем перевод строки
+                    stringWriter.flush(); // Принудительная запись в файл
+                }
+
             }
         } catch (IOException e) {
-            e.printStackTrace();
+            System.err.println("Ошибка при работе с файлами: " + e.getMessage());
         }
-
-        // можно ДОБАВЛЯТЬ в файл, применив в конструкторе append
-        try (FileWriter writer = new FileWriter(
-                "C:\\Users\\Maximus\\Desktop\\work\\SHIFT Java\\Task\\CFT-filter-utility\\files\\output\\output.txt")) {
-            for (String line : lines) {
-                writer.write(line);
-                writer.write(System.lineSeparator()); // Добавляем перевод строки
-                writer.flush(); // Принудительная запись в файл
-            }
-        } catch (IOException e) {
-            System.err.println("Ошибка записи в файл: " + e.getMessage());
-        }
-
 
 
     }
